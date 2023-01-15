310bce1d1f6d2784d2cc3f54f6eba35618386c92
Maxim Aliseiko <maliseiko@de.adit-jv.com>
2023-01-15 13:27:08 +0100

    Initial commit

---
+++ words.py
@@ -0,0 +1,27 @@
+import json
+from random import choice
+import argparse
+import json
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+                    prog = 'Words learning',
+                    description = 'Helper for learning')
+    parser.add_argument("-i", "--input", nargs="+")
+    args = parser.parse_args()
+
+    words = {}
+    for f_path in args.input:
+        with open(f_path, 'r') as f:
+            words.update(json.load(f))
+
+    while True:
+        show = choice(list(words.keys()))
+        print(f"{show}")
+        res = input()
+        if res != words[show]:
+            print("\033[1;31;40m Wrong \033[0m")
+        else:
+            print("\033[1;32;40m Correct \033[0m")
+
